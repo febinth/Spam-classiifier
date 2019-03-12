b92632c91f46f9d1b84e6f0478dea01c2ee2fc84
febout <42401629+febout@users.noreply.github.com>
2019-03-13 00:26:53 +0530

    Create mainFile.m

---
+++ mainFile.m
@@ -0,0 +1,67 @@
+%Spam Classification with SVMs
+
+%% Initialization
+clear ; close all; clc
+
+%% ==================== Email Preprocessing ====================
+% Code for preprocessing emails.
+
+fprintf('\nPreprocessing sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+
+% Print Stats
+fprintf('Word Indices: \n');
+fprintf(' %d', word_indices);
+fprintf('\n\n');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% ==================== Feature Extraction ====================
+%  Convert each email into a vector of features in R^n. 
+
+fprintf('\nExtracting features from sample email (emailSample1.txt)\n');
+
+% Extract Features
+file_contents = readFile('emailSample1.txt');
+word_indices  = processEmail(file_contents);
+features      = emailFeatures(word_indices);
+
+% Print Stats
+fprintf('Length of feature vector: %d\n', length(features));
+fprintf('Number of non-zero entries: %d\n', sum(features > 0));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%% =========== Train Linear SVM for Spam Classification ========
+%  Train a linear classifier to determine if an
+%  email is Spam or Not-Spam.
+
+load('spamTrain.mat');
+
+fprintf('\nTraining Linear SVM (Spam Classification)\n')
+fprintf('(this may take 1 to 2 minutes) ...\n')
+
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+p = svmPredict(model, X);
+
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
+
+%% =================== Test Spam Classification ================
+%  After training the classifier, we can evaluate it on a test set.
+
+% Load the test dataset
+load('spamTest.mat');
+
+fprintf('\nEvaluating the trained Linear SVM on a test set ...\n')
+
+p = svmPredict(model, Xtest);
+
+fprintf('Test Accuracy: %f\n', mean(double(p == ytest)) * 100);
+pause;
